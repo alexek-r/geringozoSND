cfc73371151b3992e10b536bf4955bb570d4359f
arodriguez <alexis.rodriguez@sondeos.com.ar>
2021-01-21 17:51:59 -0300

    arrancando

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const geringozo = function(str){
 
     /* YOUR CODE HERE */
 
+    let array = str.split("");
+
+    
+    for(let i=0; i<array.length ; i++){
+
+        if(array[i] == "a" || array[i] == "e"){
+
+        }
+
+    }
 }
 
 module.exports = geringozo
